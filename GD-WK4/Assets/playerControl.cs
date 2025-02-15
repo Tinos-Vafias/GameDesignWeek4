750f395e7580644b117ece6ddab8b952b1b4fce2
castrel17 <eac9917@nyu.edu>
2025-02-15 08:44:10 -0500

    update player control and light settings

---
+++ GD-WK4/Assets/playerControl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float movSpeed;
+    float speedX, speedY;
+    Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * movSpeed;
+        rb.linearVelocity= new Vector2(speedX, speedY);
+    }
+}
