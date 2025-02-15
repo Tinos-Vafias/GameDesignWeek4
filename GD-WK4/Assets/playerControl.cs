0b8b133d45b47ce843be47e5fae1743d0736bc5e
castrel17 <eac9917@nyu.edu>
2025-02-15 11:08:08 -0500

    fixed health bar, implemented boost and cool down for lights
    UI bar for boost is not showing up

--- GD-WK4/Assets/playerControl.cs
+++ GD-WK4/Assets/playerControl.cs
@@ -1,24 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControl : MonoBehaviour
 {
     public float movSpeed;
     float speedX, speedY;
+    [SerializeField] private Slider HPBar;
+
+    public float health;
     Rigidbody2D rb;
+    public float maxHealth;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        health = maxHealth;
     }
 
     // Update is called once per frame
+    public void updateHealthBar(float currHealth, float maxHealth){
+        HPBar.value = currHealth/maxHealth;
+    }
+
     void Update()
     {
         speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
         speedY = Input.GetAxisRaw("Vertical") * movSpeed;
         rb.linearVelocity= new Vector2(speedX, speedY);
+
+        if (Input.GetKey(KeyCode.RightShift))
+        {
+           takeDamage(0.1f);
+        }
+        HPBar.value = (health/maxHealth)*100;
+        
+    }
+
+    
+    public void takeDamage(float damageAmount){
+        health -= damageAmount;
+        if (health <- 0){
+            //they die
+        }
     }
 }
--- GD-WK4/Assets/spotLight.cs
+++ GD-WK4/Assets/spotLight.cs
@@ -1,20 +1,32 @@
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class SpotlightFollow2D : MonoBehaviour
 {
     public Transform player; 
     public float lightIntensityIncrease = 0.5f; 
-    public float maxIntensity = 2f; //max bright
-    public float minIntensity = 0.5f; //min bright
-    public float radiusIncrease = 1f; //inc factor
-    public float maxRadius = 5f; //max radius
-    public float minRadius = 0.5f; //min radius
+    public float maxIntensity = 2f;
+    public float minIntensity = 0.5f; 
+    public float radiusIncrease = 1f; 
+    public float maxRadius = 5f;
+    public float minRadius = 0.5f; 
+
     private Light2D spotlight;
+    private float boostTime;
+    private float cooldownTimer;
+
+    [SerializeField] private Slider boostBar;
+    private bool isOnCooldown = false;
+    private float maxBoostTime = 15f;
+    private float cooldownTime = 15f;
 
     void Start()
     {
+        boostBar.gameObject.SetActive(true);
         spotlight = GetComponent<Light2D>();
+        boostBar.maxValue = maxBoostTime;
+        boostBar.value = 0;
     }
 
     void Update()
@@ -24,20 +36,39 @@ public class SpotlightFollow2D : MonoBehaviour
             transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            spotlight.intensity = Mathf.Min(spotlight.intensity + lightIntensityIncrease, maxIntensity);
-        }
-
         spotlight.intensity = Mathf.Max(spotlight.intensity - Time.deltaTime * 0.5f, minIntensity);
 
-        if (Input.GetKey(KeyCode.LeftShift))
+
+        if (Input.GetKey(KeyCode.LeftShift) && !isOnCooldown)
         {
             spotlight.pointLightOuterRadius = Mathf.Min(spotlight.pointLightOuterRadius + radiusIncrease * Time.deltaTime, maxRadius);
+            boostTime += Time.deltaTime;
+            boostBar.value = boostTime;
+            Debug.Log("BOOST IS ON " + boostTime);
         }
-        else
+        else if(isOnCooldown || !Input.GetKey(KeyCode.LeftShift))
         {
+            Debug.Log("Lights are low");
             spotlight.pointLightOuterRadius = Mathf.Max(spotlight.pointLightOuterRadius - radiusIncrease * Time.deltaTime * 2f, minRadius);
+
+            if (isOnCooldown)
+        {
+            Debug.Log("cool down is on " + cooldownTimer);
+            cooldownTimer -= Time.deltaTime;
+            if (cooldownTimer <= 0)
+            {
+                isOnCooldown = false;
+                boostTime = 0;  
+                boostBar.value = 0;
+            }
+            return;
+        }
+        }
+
+        if (boostTime >= maxBoostTime)
+        {
+            isOnCooldown = true;
+            cooldownTimer = cooldownTime;
         }
     }
 }
