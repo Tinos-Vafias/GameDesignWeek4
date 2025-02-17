ae885df036fbe88a63388c73dcb6833210399d9f
castrel17 <eac9917@nyu.edu>
2025-02-17 11:07:16 -0500

    implemented combat

--- GD-WK4/Assets/playerControl.cs
+++ GD-WK4/Assets/playerControl.cs
@@ -12,10 +12,17 @@ public class PlayerControl : MonoBehaviour
     public float health;
     Rigidbody2D rb;
     public float maxHealth;
+
+    public bool isAttacking;
+
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public Transform attackPoint;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        health = maxHealth;
+        health = maxHealth; 
+        isAttacking = false;
     }
 
     // Update is called once per frame
@@ -25,16 +32,16 @@ public class PlayerControl : MonoBehaviour
         speedY = Input.GetAxisRaw("Vertical") * movSpeed;
         rb.linearVelocity= new Vector2(speedX, speedY);
 
-        if (Input.GetKey(KeyCode.RightShift)) //to take out once we have enemies
+        if (Input.GetKeyDown(KeyCode.RightShift)) //to take out once we have enemies
         {
            takeDamage(0.1f);
         }
         HPBar.value = (health/maxHealth)*100;
         
         //--------------------------------------------NOT DONE--------------------------------------------//
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-           attack();
+            Attack();
         }
     }
 
@@ -50,8 +57,21 @@ public class PlayerControl : MonoBehaviour
             Debug.Log("player died");
         }
     }
-
-    public void attack(){
+    public void Attack(){
         Debug.Log("player attacking");
+        //detect enemies that are in range of attack and store the enemies
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        foreach(Collider2D enemy in hitEnemies){ //apply damage to the enemies
+            Debug.Log("we hit " + enemy.name);
+            enemy.GetComponent<enemyControl>().TakeDamage(1);
+        }
+        
+    }
+
+    void OnDrawGizmosSelected(){
+        if(attackPoint == null){
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
--- GD-WK4/Assets/spotLight.cs
+++ GD-WK4/Assets/spotLight.cs
@@ -44,16 +44,16 @@ public class SpotlightFollow2D : MonoBehaviour
             spotlight.pointLightOuterRadius = Mathf.Min(spotlight.pointLightOuterRadius + radiusIncrease * Time.deltaTime, maxRadius);
             boostTime += Time.deltaTime;
             boostBar.value = boostTime;
-            Debug.Log("BOOST IS ON " + boostTime);
+            //Debug.Log("BOOST IS ON " + boostTime);
         }
         else if(isOnCooldown || !Input.GetKey(KeyCode.LeftShift))
         {
-            Debug.Log("Lights are low");
+            //Debug.Log("Lights are low");
             spotlight.pointLightOuterRadius = Mathf.Max(spotlight.pointLightOuterRadius - radiusIncrease * Time.deltaTime * 2f, minRadius);
 
             if (isOnCooldown)
         {
-            Debug.Log("cool down is on " + cooldownTimer);
+            //Debug.Log("cool down is on " + cooldownTimer);
             cooldownTimer -= Time.deltaTime;
             boostBar.value = cooldownTimer;
             if (cooldownTimer <= 0)
